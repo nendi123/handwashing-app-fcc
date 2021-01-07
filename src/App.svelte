c2877edd5e91a831b3bf5273b66e6c2c7da67c36
nendi123 <nendi@niagateknologi.net>
2021-01-07 14:04:27 +0700

    HTML dan komponen

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,18 @@
+<script>
+	import Timer from './Timer.svelte'
+	import HowTo from './HowTo.svelte'
+</script>
+<style>
+	h1, a {
+		text-align: center;
+	}
+</style>
 <h1>
-	Test
+	Handwashing App
 </h1>
+
+<Timer/>
+<HowTo/>
+<a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+Picture Source
+</a>
---
+++ src/ProgressBar.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let progress = 0
+</script>
+
+<style>
+    .progress-container{
+        margin: 20px 0;
+        border: solid rgb(197, 197, 197) 1px;
+    }
+    .progress-bar{
+        height: 30px;
+        background-color: rgb(77, 195, 55);
+        
+    }
+</style>
+
+<div bp="grid">
+    <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+      <div class="progress-bar" style="width: {progress}%;">
+        <span class="sr-only">%{progress}</span>
+      </div>
+    </div>
+  </div>
